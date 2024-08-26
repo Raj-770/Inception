e85b906db14ebcdf209ef7d067ee5dcbd9e1cce8
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-26 14:19:59 +0200

    updated script and Dockerfile and deleted config
    - Remove the php config file
    - updated the script to create a wp config file, install wp and create a
      user
    - Updated the Dockerfile to create new log directories and change
      ownership and users.

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,48 +1,34 @@
 #!/bin/bash
 
-
-
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
-
+# Navigate to the WordPress directory
 cd /var/www/html
 
-
+# Ensure the directory is clean
 rm -rf *
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/local/bin/wp
-
-
+# Download WordPress using WP-CLI
 wp core download --allow-root
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# Setup WordPress configuration
+wp config create --force \
+                --url="$WP_URL" \
+                --dbname="$DB_NAME" \
+                --dbuser="$DB_USER" \
+                --dbpass="$DB_PASSWORD" \
+                --dbhost="mariadb:3306"
 
-mv /wp-config.php /var/www/html/wp-config.php
+# Install WordPress
+wp core install --url="$WP_URL" \
+                --title="$WP_TITLE" \
+                --admin_user="$WP_ADMIN_NAME" \
+                --admin_password="$WP_ADMIN_PASS" \
+                --admin_email="$WP_ADMIN_EMAIL" \
+                --skip-email
 
+# Create an additional WordPress user
+wp user create $WP_USER_NAME \
+                $WP_USER_EMAIL \
+                --user_pass="$WP_USER_PASS"
 
-sed -i -r "s/db1/$DB_NAME/1"   wp-config.php
-sed -i -r "s/user/$DB_USER/1"  wp-config.php
-sed -i -r "s/pwd/$DB_PASSWORD/1"    wp-config.php
-
-wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-
-wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
-
-
-wp theme install astra --activate --allow-root
-
-wp plugin update --all --allow-root
-
-
-
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-/usr/sbin/php-fpm7.3 -F
+# Run PHP-FPM in the foreground
+php-fpm7.3 -F
