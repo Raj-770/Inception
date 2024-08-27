5967e5df82dba2dea2cb895659802936161a338e
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-08-27 12:45:51 +0200

    update
    - updated docker file so it does not change the user
    - updated script to allow running everthing by root user

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,21 +6,21 @@ rm -rf *
 
 wp core download --allow-root
 
-wp config create --force \
-				--url="$WP_URL" \
-				--dbname="$DB_NAME" \
-				--dbuser="$DB_USER" \
-				--dbpass="$DB_PASSWORD" \
-				--dbhost="mariadb:3306"
+wp config create	--allow-root --force \
+					--url="$WP_URL" \
+					--dbname="$DB_NAME" \
+					--dbuser="$DB_USER" \
+					--dbpass="$DB_PASSWORD" \
+					--dbhost="mariadb:3306"
 
-wp core install --url="$WP_URL" \
+wp core install	--allow-root--url="$WP_URL" \
 				--title="$WP_TITLE" \
 				--admin_user="$WP_ADMIN_NAME" \
 				--admin_password="$WP_ADMIN_PASS" \
 				--admin_email="$WP_ADMIN_EMAIL" \
 				--skip-email
 
-wp user create $WP_USER_NAME \
+wp user create	--allow-root $WP_USER_NAME \
 				$WP_USER_EMAIL \
 				--user_pass="$WP_USER_PASS"
 
